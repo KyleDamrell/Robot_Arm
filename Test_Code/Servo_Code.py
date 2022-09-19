68f5372c65750f29fb4ed7901c08792baadda629
Kyle Damrell <kyle.a.damrell@gmail.com>
2022-09-18 21:21:01 -0400

    Added New Code

--- Test_Code/Servo_Code.py
+++ Test_Code/Servo_Code.py
@@ -8,27 +8,49 @@ import time
 ################################
 #
 #define GPIO pins
-direction= 22 # Direction (DIR) GPIO Pin
-step = 23 # Step GPIO Pin
-EN_pin = 24 # enable pin (LOW to enable)
+direction_black= 22 # Direction (DIR) GPIO Pin
+step_black = 23 # Step GPIO Pin
+EN_pin_black = 24 # enable pin (LOW to enable)
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
-GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
+mymotortest = RpiMotorLib.A4988Nema(direction_black, step_black, (21,21,21), "DRV8825")
+GPIO.setup(EN_pin_black,GPIO.OUT) # set enable pin as output
 
 ###########################
 # Actual motor control
 ###########################
 #
-GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
+GPIO.output(EN_pin_black,GPIO.LOW) # pull enable to low to enable motor
 mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
                      "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                     200, # number of steps
-                     .0005, # step delay [sec]
-                     False, # True = print verbose output
-                     .05) # initial delay [sec]
+                     6400, # number of steps
+                     .00001, # step delay [sec]
+                     True, # True = print verbose output
+                     .00001) # initial delay [sec]
 
 GPIO.cleanup() # clear GPIO allocations after run
 
-# I am adding this as a test
-#asdf
+# Red
+direction_red = 19 # Direction (DIR) GPIO Pin
+step_red = 16 # Step GPIO Pin
+EN_pin_red = 20 # enable pin (LOW to enable)
+
+# Declare a instance of class pass GPIO pins numbers and the motor type
+mymotortest = RpiMotorLib.A4988Nema(direction_red, step_red, (21,21,21), "DRV8825")
+GPIO.setup(EN_pin_red,GPIO.OUT) # set enable pin as output
+
+print ("Done Black")
+
+# ###########################
+# # Actual motor control
+# ###########################
+# #
+# GPIO.output(EN_pin_red,GPIO.LOW) # pull enable to low to enable motor
+# mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
+#                      "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+#                      200, # number of steps
+#                      .0005, # step delay [sec]
+#                      False, # True = print verbose output
+#                      .0001) # initial delay [sec]
+# 
+# print ("Done Red")
---
+++ Test_Code/Xbox_Remote.py
@@ -0,0 +1,37 @@
+#import evdev
+from evdev import InputDevice, categorize, ecodes
+
+#cree un objet gamepad | creates object gamepad
+gamepad = InputDevice('/dev/input/event2')
+
+#affiche la liste des device connectes | prints out device info at start
+print(gamepad)
+
+#affiche les codes interceptes |  display codes
+for event in gamepad.read_loop():
+    #Boutons | buttons 
+    if event.type == ecodes.EV_KEY:
+         print("this is an event", event)
+         print(type(event))
+         string = str(event)
+         print("string", string[1])
+
+      
+        
+        
+    #Gamepad analogique | Analog gamepad
+    elif event.type == ecodes.EV_ABS:
+        absevent = categorize(event)
+        print ("Full", ecodes.bytype[absevent.event.type][absevent.event.code], absevent.event.value)
+        print("ecode", ecodes.bytype[absevent.event.type][absevent.event.code])
+        print("Value", absevent.event.value)
+        
+        
+        
+        
+        
+#         if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_X":
+#             if absevent.event.value < 30000:
+#                 print("Left Stick Up",absevent.event.value)
+#             if absevent.event.value > 30000:
+#                 print("Left Stick down",absevent.event.value) 
